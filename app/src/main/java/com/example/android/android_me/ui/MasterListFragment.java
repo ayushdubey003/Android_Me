f52496f75970be9f95bcfc89b904863738ec875a
ayushdubey003 <codeayush1998@gmail.com>
2018-12-27 00:18:48 +0530

    Hallfway there

---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,28 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+import java.util.List;
+
+public class MasterListFragment extends Fragment {
+    public MasterListFragment() {
+        //do nothing
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView=inflater.inflate(R.layout.fragment_master_list,container,false);
+        List< Integer > imageId= AndroidImageAssets.getAll();
+        MasterListAdapter masterListAdapter=new MasterListAdapter(getContext(),imageId);
+        return rootView;
+    }
+}
