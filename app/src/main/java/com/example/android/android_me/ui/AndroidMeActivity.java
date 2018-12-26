13c0eca43e7def62e9da7592762a3bf588c40ffe
ayushdubey003 <codeayush1998@gmail.com>
2018-12-26 23:15:59 +0530

    Rectify error in fragment display

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.android_me.ui;
 
@@ -35,8 +35,10 @@ public class AndroidMeActivity extends AppCompatActivity {
         // Create a new head BodyPartFragment
         BodyPartFragment headFragment = new BodyPartFragment();
 
+        // Set the list of image id's for the head fragment and set the position to the second image in the list
         headFragment.setImageIds(AndroidImageAssets.getHeads());
-        headFragment.setmIndex(1);
+        headFragment.setListIndex(5);
+
         // Add the fragment to its container using a FragmentManager and a Transaction
         FragmentManager fragmentManager = getSupportFragmentManager();
 
@@ -44,7 +46,10 @@ public class AndroidMeActivity extends AppCompatActivity {
                 .add(R.id.head_container, headFragment)
                 .commit();
 
+        // Create and display the body and leg BodyPartFragments
+
         BodyPartFragment bodyFragment = new BodyPartFragment();
+        bodyFragment.setListIndex(5);
         bodyFragment.setImageIds(AndroidImageAssets.getBodies());
         fragmentManager.beginTransaction()
                 .add(R.id.body_container, bodyFragment)
@@ -55,5 +60,7 @@ public class AndroidMeActivity extends AppCompatActivity {
         fragmentManager.beginTransaction()
                 .add(R.id.leg_container, legFragment)
                 .commit();
+
+
     }
 }
